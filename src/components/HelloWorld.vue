72864603f03a37732e61bc00a8957a02622b3e67
HY\harry <hy546880109@qq.com>
2023-03-02 18:09:32 +0800

    add

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,24 +2,51 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
 
-    <input type="text" placeholder="请输入你的问题" ref="myInput">
-    <button @click="showInputValue">提交</button>
+    <input type="text" placeholder="请输入你的问题" v-model="myInput" />
+    <button @click="getOpenAi">提交</button>
+    <h2>{{ myInput }}</h2>
+
+    <hr />
+    <!--这个标签的涵义就是一条水平的直线，但是不灵活，现在基本没人使用！-->
+    <textarea name="re" id="ai" cols="150" rows="10" v-model="messages"></textarea>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-    msg: String
+    msg: String,
+  },
+  data() {
+    return {
+      myInput: "",
+      messages: "",
+    };
   },
   methods: {
     showInputValue() {
-      console.log(this.$refs.myInput.value)
-      return this.$refs.myInput.value
-    }
-  }
-}
+      console.log(this.myInput);
+      return this.myInput;
+    },
+    async getOpenAi() {
+      // 解构赋值
+      const {data: res} = await axios({
+        method: "POST",
+        url: "https://api.openai.com/v1/chat/completions",
+        headers: {"Content-Type": "application/json", "Authorization": "Bearer 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"},
+        data: {
+          model: "gpt-3.5-turbo",
+          messages: [{ role: "user", content: "Hello!" }],
+        },
+      });
+      let messages = res.choices[0]['message']['content'];
+      console.log(messages);
+      return messages
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
+import axios from 'axios' 
 createApp(App).mount('#app')
+axios.defaults.baseURL = 'https://api.openai.com'
+App.prototype.$http = axios
